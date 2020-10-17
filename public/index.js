f2e21090836c19c4ddb91b0db333db4fd4498a8d
Brenda <brendycm@gmail.com>
2020-10-17 21:49:09 +0200

    Fixed minor issue

---
+++ public/index.js
@@ -0,0 +1,56 @@
+const infoDiv = document.getElementById('infoDiv');
+// Create a function to show fetched data
+function showData(data) {
+  const date = new Date().toDateString();
+  infoDiv.innerHTML = `
+  <h4> ${date} </h4> 
+  <p class="cityTitle"><img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" crossorigin="anonymous">${data.name},${data.sys.country}</p>
+  <span>${data.main.temp}°C</span>
+  <div class="thermalInfo">
+  <p> ${data.weather[0].main}, ${data.weather[0].description}.</p>
+  <p> Humidity: ${data.main.humidity}% </p> 
+  <p>Feels like: ${data.main.feels_like}°C</p>
+  <p>Max Temperature: ${data.main.temp_max}°C</p>
+  <p>Min Temperature: ${data.main.temp_min}°C</p>
+  <p> Wind speed : ${data.wind.speed} m/s</p>
+  <p>Wind direction: ${data.wind.deg} degrees </p> 
+  </div>
+  `;
+}
+
+// Create function to get city name from input and return information used in showData
+function getCity() {
+  const cityButton = document.getElementById('sendCity');
+  cityButton.addEventListener('click', () => {
+    const cityValue = document.getElementById('cityName').value;
+    fetch(`http://localhost:3000/search/${cityValue}`)
+      .then(res => res.json())
+      .then(data => showData(data))
+      .catch(() => {
+        infoDiv.innerHTML = 'Sorry, which planet is this city on?';
+      });
+  });
+}
+
+getCity();
+
+// Create function to show Nearby forecast with HTML geolocation settings
+function showNearby() {
+  function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    fetch(`http://localhost:3000/nearby/?lat=${lat}&lon=${lon}`)
+      .then(res => res.json())
+      .then(data => showData(data))
+      .catch(() => {
+        infoDiv.innerHTML = 'Oops, no weather detected in your location!';
+      });
+  }
+
+  function error() {
+    infoDiv.innerHTML = 'Well done! I would not reveal my location either!';
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+showNearby();
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+const fetch = require('node-fetch');
+
+const userId = process.env.APP_ID;
+
+app.use(express.static('public'));
+// First endpoint with dynamic path to use city name
+app.get('/search/:city', (req, res) => {
+    const cityName = req.params.city;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${userId}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(data => {
+            res.status(404).send(data);
+        });
+});
+
+// Second endpoint to fetch nearby forecast from API
+app.get('/nearby', (req, res) => {
+    const { lat, lon } = req.query;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${userID}&units=metric`)
+        .then(response => response.json())
+        .then(data => res.status(200).send(data))
+        .catch(data => res.status(404).send(data));
+});
+
+app.listen(port, () => console.log('Server listening on port ' + port));
