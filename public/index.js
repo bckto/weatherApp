3fa1a7ffc65894c8d72d696e6b66ef7b99e360a5
Brenda <brendycm@gmail.com>
2021-03-21 13:50:31 +0100

    removed old lint config, added prettier and fixed audit issues

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
-const infoDiv = document.getElementById('infoDiv');
-// Create a function to show fetched data
+const infoDiv = document.getElementById('infoDiv')
+    // Create a function to show fetched data
 function showData(data) {
-  const date = new Date().toDateString();
-  infoDiv.innerHTML = `
+    const date = new Date().toDateString()
+    infoDiv.innerHTML = `
   <h4> ${date} </h4> 
   <p class="cityTitle"><img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" crossorigin="anonymous">${data.name},${data.sys.country}</p>
   <span>${data.main.temp}°C</span>
@@ -15,42 +15,42 @@ function showData(data) {
   <p> Wind speed : ${data.wind.speed} m/s</p>
   <p>Wind direction: ${data.wind.deg} degrees </p> 
   </div>
-  `;
+  `
 }
 
 // Create function to get city name from input and return information used in showData
 function getCity() {
-  const cityButton = document.getElementById('sendCity');
-  cityButton.addEventListener('click', () => {
-    const cityValue = document.getElementById('cityName').value;
-    fetch(`http://localhost:3000/search/${cityValue}`)
-      .then(res => res.json())
-      .then(data => showData(data))
-      .catch(() => {
-        infoDiv.innerHTML = 'Sorry, which planet is this city on?';
-      });
-  });
+    const cityButton = document.getElementById('sendCity')
+    cityButton.addEventListener('click', () => {
+        const cityValue = document.getElementById('city-name').value
+        fetch(`http://localhost:3000/search/${cityValue}`)
+            .then((res) => res.json())
+            .then((data) => showData(data))
+            .catch(() => {
+                infoDiv.innerHTML = 'Sorry, which planet is this city on?'
+            })
+    })
 }
 
-getCity();
+getCity()
 
 // Create function to show Nearby forecast with HTML geolocation settings
 function showNearby() {
-  function success(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    fetch(`http://localhost:3000/nearby/?lat=${lat}&lon=${lon}`)
-      .then(res => res.json())
-      .then(data => showData(data))
-      .catch(() => {
-        infoDiv.innerHTML = 'Oops, no weather detected in your location!';
-      });
-  }
+    function success(position) {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        fetch(`http://localhost:3000/nearby/?lat=${lat}&lon=${lon}`)
+            .then((res) => res.json())
+            .then((data) => showData(data))
+            .catch(() => {
+                infoDiv.innerHTML = 'Oops, no weather detected in your location!'
+            })
+    }
 
-  function error() {
-    infoDiv.innerHTML = 'Well done! I would not reveal my location either!';
-  }
-  navigator.geolocation.getCurrentPosition(success, error);
+    function error() {
+        infoDiv.innerHTML = 'Well done! I would not reveal my location either!'
+    }
+    navigator.geolocation.getCurrentPosition(success, error)
 }
 
-showNearby();
+showNearby()
